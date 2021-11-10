953cf72b8dd12f039e1d479e535a9578d168939f
Andriy Kobetiak <andriykobetiak@gmail.com>
2021-11-10 10:09:48 +0200

    Delete do...while loop

--- lab_5.5/lab_5.5.cpp
+++ lab_5.5/lab_5.5.cpp
@@ -8,46 +8,22 @@ int findX(double a, double b, double eps, int deep);
 
 int main()
 {
-    double a, b, eps, m, f1, f2;
+    double a, b, eps;
     int deep = 0;
+
     cout << "a = "; cin >> a;
     cout << "b = "; cin >> b;
     cout << "eps = "; cin >> eps;
     cout << endl;
 
-    do {
-        deep++;
-
-        f1 = f(a);
-        m = (a + b) / 2;
-        f2 = f(m);
-
-        //if (f2 == 0) break;
-
-        if (f1 * f2 < 0)
-            b = m; // відкидаємо праву частину
-        else
-        {
-            a = m; // відкидаємо ліву частину
-        }
-    } while (abs(a - b) >= eps);
-
-    m = (a + b) / 2;
-    cout << "x (while):" << m << endl
-        << "Deep: " << deep << endl;
-
-    cout << "a = "; cin >> a;
-    cout << "b = "; cin >> b;
-
-    deep = 0;
-    cout << "Deep (rec): " << findX(a, b, eps, deep);
+    cout << "Deep: " << findX(a, b, eps, deep);
 
 
 }
 
 int findX(double a, double b, double eps, int deep)
 {
-    double m = 0, f1, f2;
+    double m, f1, f2;
     f1 = f(a);
     m = (a + b) / 2;
     f2 = f(m);
@@ -62,7 +38,7 @@ int findX(double a, double b, double eps, int deep)
     }
     else
     {
-        cout << "x (rec): " << m << endl;
+        cout << "x: " << m << endl;
         return deep;
     }
 }
